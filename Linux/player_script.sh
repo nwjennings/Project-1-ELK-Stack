879d09dc8c24e898d02d22a2af7044c0208706c5
nwjennings <nicholas.berton@outlook.com>
2021-08-16 23:12:19 +1000

    ELK

--- Linux/player_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-awk '{print $1, $2, $5, $6}' 0310_Dealer_schedule | egrep '05:00:00 AM|08:00:00 AM|02:00:00 PM|08:00:00 PM|11:00:00PM' >> Test1
-
-awk '{print $1, $2, $5, $6}' 0312_Dealer_schedule | egrep '05:00:00 AM|08:00:00 AM|02:00:00 PM|08:00:00 PM|11:00:00PM' >> Test1
-
-awk '{print $1, $2, $5, $6}' 0315_Dealer_schedule | egrep '05:00:00 AM|08:00:00 AM|02:00:00 PM' >> Test1
--- Linux/roulette_dealer_finder_by_time.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk '{print $1, $2, $5, $6}' $1 | grep $2 | grep $3
